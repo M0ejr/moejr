edf8004b7b79cdda3ba03bf3a1533070a0300ea0
Moejr <mo3jrr@gmail.com>
2024-02-25 19:45:41 +0300

    Changed some colors

--- src/utils/Themes.jsx
+++ src/utils/Themes.jsx
@@ -1,12 +1,12 @@
 export const darkTheme = {
-  bg: "#000b14",
-  bgLight: "#006bad",
-  primary: "#00788d",
+  bg: "#000816",
+  bgLight: "#00497a",
+  primary: "#074783",
   text_primary: "#F2F3F4",
   text_secondary: "#b6b6b6",
-  card: "#002436",
-  card_light: "#00080f",
-  button: "#0f2631",
+  card: "#001530",
+  card_light: "#000716",
+  button: "#000000",
   white: "#FFFFFF",
   black: "#000000",
 };
